5d94a3a03bb25076b632bfbe9a527cb7d9be5b7b
Thalia <thalia.rowan@gmail.com>
2020-10-21 00:01:13 -0300

    created server and 2 routes

---
+++ Develop/public/server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+
+var app = express();
+var PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get("/", function(req, res) {
+
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+  
+app.get("/notes", function(req, res) {
+  
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+  
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
